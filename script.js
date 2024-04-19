8629631446e828fac3575e231dd30ffa0257c26a
Benjamin Zeng <benjaminzeng2110@gmail.com>
2024-04-19 17:26:24 +0800

    Finish portfolio V1

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggerMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+}
